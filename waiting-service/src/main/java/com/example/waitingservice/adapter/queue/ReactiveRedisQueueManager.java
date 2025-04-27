1ee17da72e07183ed310424fce9c7eed9fdce5e6
pjh612 <pjh_jn@naver.com>
2025-04-27 20:21:44 +0900

    대기열 입장객 만료 시간 추가 (1day)

--- waiting-service/src/main/java/com/example/waitingservice/adapter/queue/ReactiveRedisQueueManager.java
+++ waiting-service/src/main/java/com/example/waitingservice/adapter/queue/ReactiveRedisQueueManager.java
@@ -6,11 +6,11 @@ import com.example.waitingservice.domain.model.UserPosition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Range;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.time.Instant;
 
 @Component
@@ -31,7 +31,9 @@ public class ReactiveRedisQueueManager implements QueueManager {
                         .filter(i -> i)
                         .switchIfEmpty(Mono.error(new RuntimeException("Failed to register in queue")))
                 )
-                .flatMap(i -> getOrder(queueName, id));
+                .flatMap(i -> redisTemplate.expire(key, Duration.ofDays(1))
+                        .then(getOrder(queueName, id))
+                );
     }
 
     @Override
